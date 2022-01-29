4f68052978e3ab7c67da2e51c0694b54c6bd4b12
Indulgense <indulgense74@gmail.com>
2022-01-29 12:51:35 +0400

    3.1.2.

---
+++ src/main/java/com/mf/spring/springmvsboot/database/DataBaseInit.java
@@ -0,0 +1,35 @@
+package com.mf.spring.springmvsboot.database;
+
+import com.mf.spring.springmvsboot.model.Role;
+import com.mf.spring.springmvsboot.model.User;
+import com.mf.spring.springmvsboot.service.RoleService;
+import com.mf.spring.springmvsboot.service.UserService;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class DataBaseInit {
+
+    private final UserService userService;
+    private final RoleService roleService;
+
+    public DataBaseInit(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @PostConstruct
+    private void addUsersInDB() {
+        User admin = new User("root", "root", 99, "root");
+        User user = new User("user", "user", 15, "user");
+        Role adminRole = new Role("ROLE_ADMIN");
+        Role userRole = new Role("ROLE_USER");
+        roleService.addRole(adminRole);
+        roleService.addRole(userRole);
+        admin.setOneRole(adminRole);
+        user.setOneRole(userRole);
+        userService.addUser(admin);
+        userService.addUser(user);
+    }
+}
---
+++ src/main/java/com/mf/spring/springmvsboot/model/Role.java
@@ -0,0 +1,89 @@
+package com.mf.spring.springmvsboot.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+
+@Entity
+@Table(name = "roles")
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "role")
+    private String role;
+
+    @ManyToMany(mappedBy = "roles")
+    Set<User> users;
+
+    public Role() {
+    }
+
+    public Role(String role) {
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+
+    public void addUserToRoles(User user){
+        if (users==null){
+            users = new HashSet<>();
+        }
+        users.add(user);
+    }
+
+    @Override
+    public String getAuthority() {
+        return getRole();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role1 = (Role) o;
+        return role.equals(role1.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(role);
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
